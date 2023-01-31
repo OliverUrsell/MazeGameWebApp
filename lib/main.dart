b2ba9f393dfa605282ed6ed6ffcc6136cddb8257
OliverUrsell <ollieursell@hotmail.co.uk>
2023-01-31 21:31:16 +0000

    Add a better background colour and a nice code textbox to the landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maze_game_web_app/game.dart';
-
-import 'maze_socket.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,32 +38,104 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController();
+    TextEditingController controller = TextEditingController(
+      text: "----",
+    );
+
+    const InputBorder textBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 1.0,
+      ),
+    );
+
+    const InputBorder focusedTextBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 3.0,
+      ),
+    );
+
+    // The position the code ends and the hyphens start
+    int codeStart = 0;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-                "Enter Code:"
-            ),
-            TextField(
-              controller: controller,
-              maxLength: 4,
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Game(code: controller.text);
-                  }));
-                },
-                child: const Text(
-                    "Connect"
-                )
-            )
-          ],
-        ),
+      backgroundColor: const Color.fromRGBO(50, 73, 80, 1.0),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Enter Code:",
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 32
+                ),
+              ),
+              Container(
+                width: 48*4 + 20*2,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    // for below version 2 use this
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                    // for version 2 and greater you can also use this
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(4),
+                  ],
+                  controller: controller,
+                  showCursor: false,
+                  style: GoogleFonts.azeretMono(
+                    textStyle: const TextStyle(
+                        fontFeatures: [
+                          FontFeature.tabularFigures()
+                        ],
+                        fontSize: 48,
+                        letterSpacing: 20,
+                    ),
+                  ),
+                  decoration: const InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: textBorder,
+                    focusedBorder: focusedTextBorder,
+                  ),
+                  onChanged: (text) {
+                    // text in this case is just the number that has been
+                    // entered (e.g. "1", "12", "1234")
+                    // The value of this parameter never contains any _ characters
+                    controller.text = text;
+                    for(int i=0; i < 4 - text.length; i++){
+                      controller.text += "-";
+                    }
+                    controller.selection = TextSelection.fromPosition(TextPosition(offset: text.length));
+                    codeStart = text.length;
+                  },
+                  onTap: () {
+                    controller.selection = TextSelection.fromPosition(TextPosition(offset: codeStart));
+                  },
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+
+                    if(codeStart != 4) {
+                      return;
+                    }
+
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return Game(code: controller.text);
+                    }));
+                  },
+                  child: const Text(
+                      "Connect"
+                  )
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
