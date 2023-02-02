b1df1494d3982b646a526eab9446b90c46bb7f46
OliverUrsell <ollieursell@hotmail.co.uk>
2023-02-02 11:11:30 +0000

    Add better styles and spacing to the landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_glow/flutter_glow.dart';
 import 'package:maze_game_web_app/game.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -34,48 +35,75 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  TextEditingController controller = TextEditingController(
+    text: "----",
+  );
 
+  InputBorder textBorder = const OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.black,
+      width: 1.0,
+    ),
+  );
+
+  InputBorder focusedTextBorder = const OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.black,
+      width: 3.0,
+    ),
+  );
+
+  TextStyle redGameNameStyle = GoogleFonts.limelight(
+    textStyle: const TextStyle(
+        color: Color.fromRGBO(230, 194, 50, 1),
+        fontSize: 96
+    ),
+  );
+
+  TextStyle goldGameNameStyle = GoogleFonts.limelight(
+    textStyle: const TextStyle(
+        color: Color.fromRGBO(118, 4, 10, 1),
+        fontSize: 96
+    ),
+  );
+
+  int codeStart = 0;
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController(
-      text: "----",
-    );
-
-    const InputBorder textBorder = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.black,
-        width: 1.0,
-      ),
-    );
-
-    const InputBorder focusedTextBorder = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.black,
-        width: 3.0,
-      ),
-    );
-
     // The position the code ends and the hyphens start
-    int codeStart = 0;
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(50, 73, 80, 1.0),
+      backgroundColor: codeStart==4 ? Colors.black : const Color.fromRGBO(50, 73, 80, 1.0),
+      // backgroundColor: const Color.fromRGBO(13, 16, 74, 1.0),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              const Text(
-                "Enter Code:",
-                style: TextStyle(
-                  color: Colors.white60,
-                  fontSize: 32
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 1/10*screenHeight, 0, 0),
+                child: GlowText(
+                  "The Falcon's Nest Hotel",
+
+                  style: codeStart==4 ? goldGameNameStyle : redGameNameStyle,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 1/10*screenHeight, 0, 0),
+                child: const Text(
+                  "Enter Code:",
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 32
+                  ),
                 ),
               ),
               Container(
                 width: 48*4 + 20*2,
+                padding: const EdgeInsets.symmetric(vertical: 30.0),
                 child: TextField(
                   keyboardType: TextInputType.number,
                   inputFormatters: [
@@ -96,12 +124,21 @@ class _MyHomePageState extends State<MyHomePage> {
                         letterSpacing: 20,
                     ),
                   ),
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     fillColor: Colors.white,
                     filled: true,
                     border: textBorder,
                     focusedBorder: focusedTextBorder,
+                    helperText: "Get this from the VR player",
+                    helperStyle: GoogleFonts.azeretMono(
+                      textStyle: const TextStyle(
+                        wordSpacing: -5,
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
                   ),
+                  autocorrect: false,
                   onChanged: (text) {
                     // text in this case is just the number that has been
                     // entered (e.g. "1", "12", "1234")
@@ -111,27 +148,45 @@ class _MyHomePageState extends State<MyHomePage> {
                       controller.text += "-";
                     }
                     controller.selection = TextSelection.fromPosition(TextPosition(offset: text.length));
-                    codeStart = text.length;
+                    setState(() {
+                      codeStart = text.length;
+                    });
                   },
                   onTap: () {
                     controller.selection = TextSelection.fromPosition(TextPosition(offset: codeStart));
                   },
                 ),
               ),
-              TextButton(
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 1/10*screenHeight, 0, 0),
+                child: TextButton(
                   onPressed: () {
-
                     if(codeStart != 4) {
                       return;
                     }
-
                     Navigator.push(context, MaterialPageRoute(builder: (context) {
                       return Game(code: controller.text);
                     }));
                   },
-                  child: const Text(
-                      "Connect"
-                  )
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            side: codeStart==4? const BorderSide(color: Colors.red):const BorderSide(color: Colors.black)
+                        )
+                    )
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Connect",
+                      style: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 32
+                      ),
+                    ),
+                  ),
+                ),
               )
             ],
           ),
